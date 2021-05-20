8cfaa59c625c87c31753c73efb390b7ea0e236d4
Stefan Rijnhart <stefan@opener.am>
2021-05-20 13:02:35 +0200

    [IMP] date_range: fully automatic generation from date range types

--- date_range/__manifest__.py
+++ date_range/__manifest__.py
@@ -14,6 +14,7 @@
     ],
     # odoo_test_helper is needed for the tests
     "data": [
+        "data/ir_cron_data.xml",
         "security/ir.model.access.csv",
         "security/date_range_security.xml",
         "views/assets.xml",
